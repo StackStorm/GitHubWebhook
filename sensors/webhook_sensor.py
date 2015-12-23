1c7f602d07681e92153d7dc3aa81b19f1e9cb755
Patrick Hoolboom <patrick@stackstorm.com>
2015-12-23 10:02:08 -0800

    add threaded=True

--- sensors/webhook_sensor.py
+++ sensors/webhook_sensor.py
@@ -68,7 +68,7 @@ class WebhookSensor(Sensor):
             return json.dumps({"response":"triggerposted"})
 
     def run(self):
-        self.app.run(host=self.host,port=self.port,debug=True)
+        self.app.run(host=self.host,port=self.port,debug=True, threaded=True)
 
     def cleanup(self):
         # This is called when the st2 system goes down. You can perform cleanup operations like
